e3c4668db05c4329802b7a59bf1f9da0d6ba5d21
Vaibhav Phutane <vaibhav.phutane@nihilent.com>
2019-10-23 13:07:53 +0530

    Latest changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,42 +6,51 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'webrtc';
   targetPeer: any;
   peer: any;
+  connectionString: string;
   @ViewChild('myVideo') myVideo: ElementRef;
-   nav = <any>navigator;
+  @ViewChild('screenVideo') screenVideo: ElementRef;
+   nav = <any> navigator;
 
    ngOnInit() {
     const video = this.myVideo.nativeElement;
     let peerx: any;
     this.nav.getUserMedia = (this.nav.getUserMedia || this.nav.webkitGetUserMedia || this.nav.mozGetUserMedia || this.nav.msGetUserMedia);
-    this.nav.getUserMedia({audio: true, video: true}, function(stream) {
+    this.nav.getUserMedia({audio: true, video: true},  (stream) => {
+
     peerx = new SimplePeer({
       initiator: location.hash === '#init',
       trickle: false,
       iceTransportPolicy: 'relay',
       stream: stream
     });
-    peerx.on('signal', function(data) {
+
+    peerx.on('signal', (data) => {
+      this.connectionString = JSON.stringify(data);
         console.log(JSON.stringify(data));
         this.targetPeer = data;
     });
-    peerx.on('data', function(data) {
+
+    peerx.on('data', (data) => {
       console.log('Received Message:' + data);
     });
-    peerx.on('stream', function (stream) {
+
+    peerx.on('stream', (stream) => {
       video.srcObject = stream;
       // video.src = URL.createObjectURL(stream);
       video.play();
     });
-  }, function (err) {
+
+  },  (err) => {
     console.log(err);
   });
     setTimeout(() => {
        this.peer = peerx;
        console.log(this.peer);
+      //  this.connectionString = this.peer;
      }, 5000);
   }
 
@@ -53,3 +62,5 @@ export class AppComponent {
     this.peer.send('Hello world');
   }
 }
+
+
